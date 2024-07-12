b80433f367099c14a0cd344b9b171e66cffd0320
Mattia Marini <marmattia26@gmail.com>
2024-07-12 19:11:46 +0200

    Added c support

--- lua/runner/config.lua
+++ lua/runner/config.lua
@@ -4,6 +4,7 @@ return {
     ["<Space>R"] = require("runner.run").stop
   },
   cpp = require("runner.defaults.cpp"),
+  c = require("runner.defaults.c"),
   lua = require("runner.defaults.lua"),
   sh = require("runner.defaults.sh")
 }
