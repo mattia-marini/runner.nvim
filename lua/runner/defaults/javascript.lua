35ae02cc0b85269d71e51a385bffad54fccc766f
Mattia Marini <marmattia26@gmail.com>
2024-08-28 15:05:41 +0200

    Fixes and feature:
    - Added default js config
    - Fixed parsion issue
    - Fixed cpp config

---
+++ lua/runner/defaults/javascript.lua
@@ -0,0 +1,6 @@
+local rv = require("runner.defaults.common").new()
+rv.buildAndRun =
+    function(args)
+      return "cd " .. args.default.currFileDir .. " && node " .. args.default.currFileName .. args.default.args
+    end
+return rv
--- lua/runner/init.lua
+++ lua/runner/init.lua
@@ -3,6 +3,7 @@ require("runner.autocmd")
 local function parseConfig(t1, t2)
   for key, val in pairs(t2) do
     if type(val) == "table" then
+      t1[key] = t1[key] and t1[key] or {}
       parseConfig(t1[key], val)
     else
       t1[key] = val
