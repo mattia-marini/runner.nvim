f2f68943b39c934bf6ba04dd30adab97497e9ab7
Mattia <marmattia26@gmail.com>
2024-12-23 14:40:57 +0100

    Changed cpp default

--- lua/runner/defaults/cpp.lua
+++ lua/runner/defaults/cpp.lua
@@ -1,6 +1,6 @@
 local rv = require("runner.defaults.common").new()
 rv.buildAndRun =
     function(args)
-      return "cd " .. args.default.currFileDir .. " && clang++ " .. args.default.currFileName .. " && ./a.out " .. args.default.args
+      return "cd " .. args.default.currFileDir .. " && clang++ -std=c++17 " .. args.default.currFileName .. " && ./a.out " .. args.default.args
     end
 return rv
