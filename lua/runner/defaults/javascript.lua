31fcc90a7943ace81a45d3e208ff7a493a2d9b20
Mattia Marini <marmattia26@gmail.com>
2025-06-07 07:58:16 +0200

    Added quotes to files path in default configs

--- lua/runner/defaults/javascript.lua
+++ lua/runner/defaults/javascript.lua
@@ -1,6 +1,6 @@
 local rv = require("runner.defaults.common").new()
 rv.buildAndRun =
     function(args)
-      return "cd " .. args.default.currFileDir .. " && node " .. args.default.currFileName .. args.default.args
+      return "cd '" .. args.default.currFileDir .. "' && node " .. args.default.currFileName .. args.default.args
     end
 return rv
