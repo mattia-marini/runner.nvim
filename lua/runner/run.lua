47a22f110dbda3416b911ba6b31ebf863d317c91
Mattia Marini <marmattia26@gmail.com>
2025-06-19 03:52:43 +0200

    Better term infos

--- lua/runner/run.lua
+++ lua/runner/run.lua
@@ -1,4 +1,4 @@
-vim.api.nvim_buf_set_var(0, "runnerTermChannelId", nil);
+vim.api.nvim_buf_set_var(0, "runnerTermInfos", nil);
 
 local function getActiveTerminalWinId()
   local wins = vim.api.nvim_list_wins()
@@ -20,30 +20,37 @@ local function runInTerminalById(winid, cmd)
 end
 
 local function runInTerminal(cmd)
-  local channel_id = vim.api.nvim_buf_get_var(0, "runnerTermChannelId")
-
-  if (channel_id ~= nil) then
+  local termInfos = vim.api.nvim_buf_get_var(0, "runnerTermInfos")
+  if (termInfos ~= nil) then
     vim.notify("A process is already running in this buffer", vim.log.levels.WARN)
     return
   end
 
-  local text_file_buff = vim.api.nvim_win_get_buf(0);
+  local channel_id = termInfos.ch
+
+  local text_file_buff = vim.api.nvim_get_current_buf();
   vim.cmd("vnew")
-  local channel_id = vim.fn.termopen(cmd)
-  vim.api.nvim_buf_set_var(text_file_buff, "runnerTermChannelId", channel_id)
+  local term_window = vim.api.nvim_get_current_win();
+
+  local new_channel_id = vim.fn.termopen(cmd)
+  vim.api.nvim_buf_set_var(text_file_buff, "runnerTermInfos", { ch = new_channel_id, win = term_window })
 end
 
 local function stopExecution()
-  local channel_id = vim.api.nvim_buf_get_var(0, "runnerTermChannelId")
-  if (channel_id == nil) then
+  local termInfos = vim.api.nvim_buf_get_var(0, "runnerTermInfos")
+  if (termInfos == nil) then
     vim.notify("No running process to stop", vim.log.levels.WARN)
     return
   end
 
-  -- vim.api.nvim_chan_send(vim.api.nvim_buf_get_var(0, "runnerTermChannelId"),
+  local channel_id = termInfos.ch
+  local term_win = termInfos.win
+
+  -- vim.api.nvim_chan_send(vim.api.nvim_buf_get_var(0, "runnerTermInfos"),
   --   vim.api.nvim_replace_termcodes("<C-c>", true, false, true))
   vim.fn.jobstop(channel_id)
-  vim.api.nvim_buf_set_var(0, "runnerTermChannelId", nil);
+  vim.api.nvim_buf_set_var(0, "runnerTermInfos", nil);
+  vim.api.nvim_win_close(term_win, false)
   --   local term = getActiveTerminalWinId()
   --
   --   local currWin = vim.api.nvim_get_current_win()
